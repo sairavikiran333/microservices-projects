5747f3abdcc07c7020affd522062f74e93bbb64c
sappalab <sappalab@SAPPALAB-7420.oradev.oraclecorp.com>
2023-03-02 18:29:25 +0530

    deleting vishymails proj

--- vishymails-projects/microservices-mar2023-master (1)/microservices-mar2023-master/nosql-mongo-service/src/main/java/com/bvr/repositories/CarMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bvr.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bvr.models.Car;
-
-public interface CarMongoRepository extends CrudRepository<Car, String> {
-
-}
